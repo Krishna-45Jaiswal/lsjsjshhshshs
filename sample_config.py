24029fa2cad9b7fce7810272c57549cb18446d16
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-03-01 10:02:22 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,11 @@
+import os
+from typing import Set
+
+from telethon.tl.types import ChatBannedRights
+from validators.url import url
+
+
+class Config(object):
+  API_ID = os.environ.get("API_ID", None) 
+  
+  
